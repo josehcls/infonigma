19b7318cea02d8b5652693bcbf51842a1449d452
José Henrique Leopoldo e Silva <josehcls@gmail.com>
2019-05-02 21:02:05 -0300

    Update cp.sh

--- deploy/cp.sh
+++ deploy/cp.sh
@@ -4,5 +4,7 @@ sudo cp infonigma.socket /etc/systemd/system/infonigma.socket
 sudo cp infonigma.conf /etc/tmpfiles.d/infonigma.conf
 sudo cp infonigma /etc/nginx/sites-available/infonigma
 sudo ln -s /etc/nginx/sites-available/infonigma /etc/nginx/sites-enabled
+mkdir /home/infonigma/logs
+touch /home/infonigma/logs/gunicorn.error
+touch /home/infonigma/logs/gunicorn.access
 echo 'Done!'
-
